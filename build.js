c47b6975d9d5a3026e3b4c8f4fd5ca14c4347f46
Jordan Giustizia <33008862+JordanGiustizia@users.noreply.github.com>
2021-04-07 02:48:41 -0400

    Making preperations for first public release

--- build.js
+++ build.js
@@ -1,15 +1,24 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const artFilePath = './src/res/img/art-compressed/';
-const artFileNames = fs.readdirSync(artFilePath, {withFileTypes: true}).filter(item => !item.isDirectory()).map(item => item.name);
+const art_path = "./src/res/img/art-comp";
 
-const htmlString =
+const art_names = fs.readdirSync(art_path, {withFileTypes: true}).filter(function(item) {
+    return !item.isDirectory();
+}).map(function(item) {
+    return item.name;
+});
+
+if (art_names[0] === ".DS_Store") {
+    art_names.shift();
+}
+
+const t__pg__showcase =
 `<!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
-        <title>deianeira</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no" />
+        <title>33rpm | deianeira</title>
         
         <link rel="stylesheet" type="text/css" href="../src/styles/index.css" />
     </head>
@@ -20,32 +29,40 @@ const htmlString =
         <header>
             <a href="#top">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82.17 61.9" width="82.17px">
-                    <circle cx="30.95" cy="30.95" r="30.6"/>
-                    <circle cx="51.22" cy="30.95" r="30.6"/>
-                    <circle cx="51.22" cy="30.95" r="10.13"/>
-                    <circle cx="30.95" cy="30.95" r="10.13"/>
-                    <circle cx="41.08" cy="30.95" r="30.6"/>
-                    <circle cx="41.08" cy="30.95" r="10.13"/>
+                    <circle cx="30.95" cy="30.95" r="30.6" />
+                    <circle cx="51.22" cy="30.95" r="30.6" />
+                    <circle cx="51.22" cy="30.95" r="10.13" />
+                    <circle cx="30.95" cy="30.95" r="10.13" />
+                    <circle cx="41.08" cy="30.95" r="30.6" />
+                    <circle cx="41.08" cy="30.95" r="10.13" />
                 </svg>
             </a>
 
-            <span>Have a project? I want to work with you: <a href="mailto:d@deianeira.co">d@deianeira.co</a></span>
+            <span><a href="mailto:d@deianeira.co">d@deianeira.co</a></span>
         </header>
 
-        <section>${artFileNames ? artFileNames.map(artFileName => `<figure><a href="../${artFilePath}${artFileName}" rel="noopener noreferrer" target="_blank"><img src="../${artFilePath}${artFileName}" loading="lazy" /></a></figure>`).join('') : ''}</section>
+        <section>${art_names.map(art_name => `<figure><a href="../${art_path}/${art_name}" rel="noopener noreferrer" target="_blank"><img src="../${art_path}/${art_name}" loading="lazy" /></a></figure>`).join('')}</section>
+    
+        <footer>
+            <span>© MMXXI <a href="https://deianeira.co">Δηϊάνειρα</a>
+            <span><a href="https://deianeira.co/elsewhere">elsewhere</a>
+            <a href="https://webring.xxiivv.com/" target="_blank" rel="noopener noreferrer">
+                <img id="webring" src="../src/res/img/webring.svg" alt="xxiivv webring" />
+            </a>
+        </footer>
     </body>
 </html>`;
 
-const writeHTMLToFile = (writePath, fileName, pageData) => {
-    const datedFileName = `${Date.now()}-${fileName}`;
+const write_html_to_file = (write_path, file_name, page_data) => {
+    const dated_file_name = `${Date.now()}-${file_name}`;
 
-    fs.writeFile(`${writePath}/${datedFileName}`, pageData, error => {
+    fs.writeFile(`${write_path}/${dated_file_name}`, page_data, error => {
         if (error) {
             throw error;
         }
 
-        console.log(`File created succesfully at ${writePath}/${datedFileName}`);
+        console.log(`File created succesfully at ${write_path}/${dated_file_name}`);
     });
 };
 
-writeHTMLToFile('./build', 'index.html', htmlString);
+write_html_to_file('./public', 'index.html', t__pg__showcase);
