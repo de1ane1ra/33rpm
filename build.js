23cf8a08c6efb890b5983f6e06769c5fff8b5b00
Jordan Giustizia <33008862+JordanGiustizia@users.noreply.github.com>
2021-04-08 07:42:59 -0400

    Making preparations to build for multiple envs/use-cases, created low-power variant of the 33rpm gallery

--- build.js
+++ build.js
@@ -1,8 +1,28 @@
 const fs = require("fs");
 
-const art_path = "./public/src/res/img/art-comp";
+const build_for = "public";
 
-const art_names = fs.readdirSync(art_path, {withFileTypes: true}).filter(function(item) {
+const envs = {
+    public: {
+        gallery_path: "comp",
+        details_path: "comp",
+        out_path: "./public"
+    },
+    low_power: {
+        gallery_path: "dith",
+        details_path: "comp",
+        out_path: "./public/low-power"
+    }
+};
+
+const img_path = "src/res/img";
+const gallery_path = envs[build_for].gallery_path;
+const details_path = envs[build_for].details_path;
+const build_path = img_path + "/" + gallery_path;
+const src_path = "public/" + build_path;
+const out_path = envs[build_for].out_path;
+
+const art_names = fs.readdirSync(src_path, {withFileTypes: true}).filter(function(item) {
     return !item.isDirectory();
 }).map(function(item) {
     return item.name;
@@ -12,7 +32,7 @@ if (art_names[0] === ".DS_Store") {
     art_names.shift();
 }
 
-const t__pg__showcase = function(image_path) {
+const t__pg__showcase = function(img_path) {
     return (
         `<!DOCTYPE html>
         <html lang="en">
@@ -21,7 +41,7 @@ const t__pg__showcase = function(image_path) {
                 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no" />
                 <title>33rpm | deianeira</title>
                 
-                <link rel="stylesheet" type="text/css" href="./src/styles/index.css" />
+                <link rel="stylesheet" type="text/css" href="${build_for === "low_power" ? "../" : "./"}src/styles/index.css" />
             </head>
             
             <body>
@@ -42,14 +62,21 @@ const t__pg__showcase = function(image_path) {
                     <span><a href="mailto:d@deianeira.co">d@deianeira.co</a></span>
                 </header>
 
-                <section>${art_names.map(art_name => `<figure><a href="./${image_path}/${art_name}" rel="noopener noreferrer" target="_blank"><img src="./${image_path}/${art_name}" loading="lazy" /></a></figure>`).join('')}</section>
+                <section>${art_names.map(art_name => `<figure><a href="${build_for === "low_power" ? "../" : "./"}${img_path}/${details_path}/${art_name}" rel="noopener noreferrer" target="_blank"><img src="${build_for === "low_power" ? "../" : "./"}${img_path}/${gallery_path}/${art_name}" loading="lazy" /></a></figure>`).join('')}</section>
             
+                <aside>
+                    <span><a href="mailto:d@deianeira.co">Is your music in need of a companion?</a></span>
+                </aside>
+
                 <footer>
-                    <span>© MMXXI <a href="https://deianeira.co">Δηϊάνειρα</a></span>
-                    <span><a href="https://deianeira.co/elsewhere">elsewhere</a></span>
-                    <a href="https://webring.xxiivv.com/" target="_blank" rel="noopener noreferrer">
-                        <img id="webring" src="./src/res/img/webring.svg" alt="xxiivv webring" />
-                    </a>
+                    ${build_for !== "low_power" ? `<span id="low-power"><a href="./low-power">switch to low power</a></span>` : ""}
+                    <div>
+                        <span>© MMXXI <a href="https://deianeira.co">Δηϊάνειρα</a></span>
+                        <span><a href="https://deianeira.co/elsewhere">elsewhere</a></span>
+                        <a href="https://webring.xxiivv.com/" target="_blank" rel="noopener noreferrer">
+                            <img id="webring" src="${build_for === "low_power" ? "../" : "./"}src/res/img/webring.svg" alt="xxiivv webring" />
+                        </a>
+                    </div>
                 </footer>
             </body>
         </html>`
@@ -57,15 +84,13 @@ const t__pg__showcase = function(image_path) {
 }
 
 const write_html_to_file = (write_path, file_name, page_data) => {
-    const dated_file_name = `${Date.now()}-${file_name}`;
-
-    fs.writeFile(`${write_path}/${dated_file_name}`, page_data, error => {
+    fs.writeFile(`${write_path}/${file_name}`, page_data, error => {
         if (error) {
             throw error;
         }
 
-        console.log(`File created succesfully at ${write_path}/${dated_file_name}`);
+        console.log(`File created succesfully at ${write_path}/${file_name}`);
     });
 };
 
-write_html_to_file('./public', 'index.html', t__pg__showcase("./src/res/img/art-comp"));
+write_html_to_file(out_path, 'index.html', t__pg__showcase(img_path));
